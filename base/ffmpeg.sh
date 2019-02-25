0d86126ead2c5d9a70c38d515bf918b4c8b6fef7
guang11cheng <guang11cheng@qq.com>
2019-02-25 19:46:24 +0800

    完善脚本

--- base/ffmpeg.sh
+++ base/ffmpeg.sh
@@ -6,4 +6,8 @@
 
 FFMPEG_BIN_DIR=/opt/third_party/release/ffmpeg/bin
 export LD_LIBRARY_PATH=${FFMPEG_BIN_DIR}/../lib:${FFMPEG_BIN_DIR}/../../fdk_aac/lib:${FFMPEG_BIN_DIR}/../../x264/lib
-${FFMPEG_BIN_DIR}/ffmpeg -i ~/Downloads/VIDEO0018.mp4 -f s16le ~/Downloads/out.pcm
+
+${FFMPEG_BIN_DIR}/ffprobe ~/Desktop/fcz2.flv
+
+# 抽取mp4中的原始音频数据
+${FFMPEG_BIN_DIR}/ffmpeg -i ~/Desktop/hgc-2018-Q3.mp4 -sample_rate 44100 -channels 2 -f s16le ~/Downloads/out.pcm
